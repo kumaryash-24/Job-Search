6bc4bc929948590af411a6b5e077cc402d81034c
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2025-08-07 10:32:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import connectDB from "./utils/db.js";
+import connectDB from "./utils/db.js";             
 import userRoutes from './routes/user.route.js';
 import jobRoutes from "./routes/job.routes.js";
 import adminRoutes from "./routes/admin.routes.js";             
